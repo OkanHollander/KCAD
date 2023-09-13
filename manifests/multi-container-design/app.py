f1db471556d6a40604f76af8f3d4e165700c67a4
Okan Hollander <okan.hollander@quality.nl>
2023-09-13 12:56:43 +0200

    updated guid

--- manifests/multi-container-design/app.py
+++ manifests/multi-container-design/app.py
@@ -4,7 +4,6 @@ import time
 import uuid
 
 LOG_PATH = "/app/logs/mytestlog.log"
-RANDOM_NUMBER = uuid.uuid4()
 MIN_SLEEP = 0.3
 MAX_SLEEP = 0.8
 
@@ -17,4 +16,4 @@ if __name__ == "__main__":
     for i in range(10000):
         random_sleep()
         with open(LOG_PATH, "a", encoding="utf-8") as f:
-            f.write(f"{datetime.datetime.now()} New GUID: {RANDOM_NUMBER}\n")
+            f.write(f"{datetime.datetime.now()} New GUID: {uuid.uuid4()}\n")
